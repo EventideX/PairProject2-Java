6f4257b77ea3bdd66f1d2558dad10dd0dde65298
sword98 <228924211@qq.com>
2019-03-11 16:55:12 +0800

    进阶v1.0

--- 221600219&221600212/src/Main.java
+++ 221600219&221600212/src/Main.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-
+import java.util.regex.Pattern;
 public class Main{
 
     // 行数
@@ -30,10 +30,29 @@ public class Main{
     // 词组的词数
     public static int phraseWordNum = 1;
 
-    // 是否词频权重
+    // 是否使用-m参数
+    public static boolean usePhraseWordNum = false;
+
+    // 是否使用-n参数
+    public static boolean useSortedPrintNum = false;
+
+    // 是否使用-w参数
     public static boolean useWordWeight = false;
-
-
+	
+	// 论文编号
+    public static String strNumber = null;
+	
+	// 论文标题
+    public static String strTitle = null;
+	
+	// 论文摘要
+    public static String strAbstract = null;
+	
+	// 论文标题单词数
+	public static int strTitleNumber = 0;
+	
+	// 论论文摘要单词书
+    public static int strAbstractNumber = 0;
     /**
      * 程序入口
      * @param args[0] 输入文件名
@@ -41,25 +60,8 @@ public class Main{
     public static void main(String[] args) {
         // 初始化
         loadArgs(args);
-        inputFileBytes = readFileToBytes(inputFileName);
-        Lib core = new Lib(inputFileBytes);
-
-        // 内核处理
-        core.preproccess();
-        core.collectWord();
-        core.sortWordMap();
-
-        // 获取结果
-        charNum = core.getCharNum();
-        wordNum = core.getWordNum();
-        lineNum = core.getLineNum();
-        wordList = core.getSortedList();
-
-        // 打印结果
-        printResult();
-
-        // 保存结果
-        writeResult();
+		inputFileBytes = readFileToBytes(inputFileName);
+        runProgress();
     }
 
     /**
@@ -79,9 +81,11 @@ public class Main{
                         break;
                     case "-m":
                         phraseWordNum = Integer.valueOf(args[++i]);
+                        usePhraseWordNum=true;
                         break;
                     case "-n":
                         sortedPrintNum = Integer.valueOf(args[++i]);
+                        useSortedPrintNum=true;
                         break;
                     case "-w":
                         useWordWeight = (args[i+1].equals("1"));
@@ -94,11 +98,11 @@ public class Main{
             System.out.println("未输入参数");
             System.exit(1);
         }
-        System.out.println(inputFileName);
-        System.out.println(outputFileName);
-        System.out.println(phraseWordNum);
-        System.out.println(sortedPrintNum);
-        System.out.println(useWordWeight);
+        //System.out.println(inputFileName);
+        //System.out.println(outputFileName);
+        //System.out.println(phraseWordNum);
+        //System.out.println(sortedPrintNum);
+        //System.out.println(useWordWeight);
     }
 
 
@@ -111,20 +115,31 @@ public class Main{
      */
     public static byte[] readFileToBytes(String fileName){
         byte[] fileBytes = null;
-
+		
         try {
-            File file = new File(fileName);
-            FileInputStream reader = new FileInputStream(file);
-            Long fileLength = file.length();
-            fileBytes = new byte[fileLength.intValue()];
-            reader.read(fileBytes);
-            for (byte b : fileBytes){
-                System.out.print((char)b);
-                System.out.print(":");
-                System.out.print(b);
-                System.out.print("/");
-            }
-            reader.close();
+			File file = new File(fileName);
+			BufferedReader reader=new BufferedReader(new FileReader(file));
+			
+			strNumber=reader.readLine();
+			//System.out.println(strNumber);
+			strTitle=reader.readLine();
+			strTitle=strTitle.substring(6).toLowerCase();
+			//System.out.println(strTitle);
+			strAbstract=reader.readLine();
+			strAbstract=strAbstract.substring(9).toLowerCase();
+			//System.out.println(strAbstract);
+			strNumber=reader.readLine();
+			String str=strTitle+"\r\n"+strAbstract;
+			fileBytes=str.getBytes();
+			
+				// for (byte b : fileBytes){
+				//     System.out.print((char)b);
+				//     System.out.print(":");
+				//     System.out.print(b);
+				//     System.out.print("/");
+				// }
+				//reader.close();
+			
         }
         catch(Exception e){
             System.out.println("读取文件出错");
@@ -139,8 +154,8 @@ public class Main{
      */
     public static void printResult(){
         System.out.println("characters: " + charNum);
-        System.out.println("words: " + lineNum);
-        System.out.println("lines: " + wordNum);
+        System.out.println("words: " + wordNum);
+        System.out.println("lines: " + lineNum);
         int i = 0;
         for (Map.Entry<String, Integer> entry : wordList) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
@@ -155,12 +170,12 @@ public class Main{
      */
     public static void writeResult(){
         String resultString = String.format(
-            "characters: %d\nwords: %d\nlines: %d\n",
+            "characters: %d\r\nwords: %d\r\nlines: %d\r\n",
             charNum, wordNum, lineNum
         );
         int i = 0;
         for (Map.Entry<String, Integer> entry : wordList) {
-            resultString += String.format("%s: %d\n", entry.getKey(), entry.getValue());
+            resultString += String.format("%s: %d\r\n", entry.getKey(), entry.getValue());
             if (++ i >= sortedPrintNum){
                 break;
             }
@@ -178,5 +193,30 @@ public class Main{
             System.exit(1);
         }
     }
+	
+	public static void runProgress(){
+		Lib core = new Lib(inputFileBytes);
 
+        // 内核处理
+        core.preproccess();
+		strTitleNumber=core.filterTitle(strTitle.getBytes());
+		System.out.println(strTitle);
+		strAbstractNumber=core.filterAbstract(strAbstract.getBytes())-strTitleNumber;
+		System.out.println(strAbstract);
+        core.collectWord(strTitle.trim().split(" "),1);
+		core.collectWord(strAbstract.trim().split(" "),2);
+        core.sortWordMap();
+
+        // 获取结果
+        charNum = core.getCharNum();
+        wordNum = core.getWordNum();
+        lineNum = core.getLineNum();
+        wordList = core.getSortedList();
+
+        // 打印结果
+        printResult();
+
+        // 保存结果
+        writeResult();
+	}
 }
