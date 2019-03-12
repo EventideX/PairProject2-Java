c970d739aa9bea8656f693c8c7a6f48609f47ecd
masgak <34679999+masgak@users.noreply.github.com>
2019-03-13 00:47:41 +0800

    自定义输入输出文件

--- 221600330&221600331/src/Main.java
+++ 221600330&221600331/src/Main.java
@@ -1,1 +1,24 @@
+import java.util.*;
 
+public class Main {
+
+	public static void main(String[] args) {
+		int countChar;
+		int countLinnes;
+		System.out.print("输入，输出文件地址：");
+		Scanner scan = new Scanner(System.in);
+		String file_name = scan.nextLine();
+		String out_name = scan.nextLine();
+		scan.close();
+		Map<String, String> wordsMap;
+		countChar = wordCount.count_Characters(file_name);
+		wordsMap = wordCount.count_Words(file_name);
+		countLinnes = wordCount.count_Lines(file_name);
+		
+		wordCount.writeToFile(countChar,wordsMap,countLinnes,out_name);
+	}
+	
+	
+
+
+}
