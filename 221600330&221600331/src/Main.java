bf66904048c4d76f7fe6a62d612ea637bc6b2278
masgak <34679999+masgak@users.noreply.github.com>
2019-03-13 18:20:12 +0800

    更改命令行输入方式，修改统计函数的错误

--- 221600330&221600331/src/Main.java
+++ 221600330&221600331/src/Main.java
@@ -3,19 +3,60 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
+		/*是否加权重*/
+		boolean isWeight=false;       
 		int countChar;
 		int countLinnes;
-		System.out.print("输入，输出文件地址：");
-		Scanner scan = new Scanner(System.in);
-		String file_name = scan.nextLine();
-		String out_name = scan.nextLine();
-		scan.close();
+		int phraseLength;
+		int numbers;
+		/*输入与输出地址*/
+		String in_name = "input.txt";
+		String out_name = "result.txt";
+		for(int i=0;i<args.length;i+=2) {
+			switch(args[i]) {
+				case "-i":
+					/*更改输入地址*/
+					in_name=args[i+1];
+					break;
+				case "-o":
+					/*更改输出地址*/
+					out_name=args[i+1];
+					break;
+				case "-w":
+					/*由输入参数选择权重占比*/
+					if(Integer.parseInt(args[i+1])==0) {
+						isWeight=true;
+					}else {
+						isWeight=false;
+					}
+					break;
+				case "-m":
+					/*统计文件夹中指定长度的词组的词频
+					 * 出现-m时只统计词组词频
+					 * 未出现-m时只统计单词词频
+					 * */
+					phraseLength=Integer.parseInt(args[i+1]);
+					break;
+				case "-n":
+					/*用户指定输出前number多的单词(词组)与其频数
+					 * 表示输出频数最多的前 [number] 个单词(词组)
+					 * */
+					numbers=Integer.parseInt(args[i+1]);
+					break;
+			}
+		}
 		Map<String, String> wordsMap;
-		countChar = wordCount.count_Characters(file_name);
-		wordsMap = wordCount.count_Words(file_name);
-		countLinnes = wordCount.count_Lines(file_name);
-		
-		wordCount.writeToFile(countChar,wordsMap,countLinnes,out_name);
+		Map<String, String> wordsFrequency;
+		/*输出文件的字符数*/
+		countChar = wordCount.count_Characters(in_name);
+		/*统计单词出现次数和单词总数*/
+		wordsMap = wordCount.count_Words(in_name);
+		/*统计行数*/
+		countLinnes = wordCount.count_Lines(in_name);
+		/*初步统计的单词(或词组)和词频的Map、数字选项*/
+		wordsFrequency=wordCount.count_Word_Frequency(in_name, wordsMap,isWeight);
+		/*输出至文件中*/
+		wordCount.writeToFile(countChar,wordsFrequency,countLinnes,out_name);
 	}
 	
 	
