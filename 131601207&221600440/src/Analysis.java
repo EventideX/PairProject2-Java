b8bd1fbe2542e25526350e869b65dde7197c0103
xiaobinghhh <46930789+xiaobinghhh@users.noreply.github.com>
2019-03-14 21:35:59 +0800

    Add files via upload
    the forward couting...

---
+++ 131601207&221600440/src/Analysis.java
@@ -0,0 +1,48 @@
+
+public class Analysis {
+	private String[] args;
+	private String inputFilePath;
+	private String outputFilePath;
+	private int weight;
+	private int phraseSize=1;//默认进行单词词频统计
+	private int resultCnt=10;//默认输出10个
+
+	public Analysis(String args[]) {
+		this.args = args;
+	}
+
+	public String getInputFilePath() {
+		return inputFilePath;
+	}
+
+	public String getOutputFilePath() {
+		return outputFilePath;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public int getPhraseSize() {
+		return phraseSize;
+	}
+
+	public int getResultCnt() {
+		return resultCnt;
+	}
+
+	public void analyse() {
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-i"))
+				inputFilePath = args[i + 1];
+			else if (args[i].equals("-o"))
+				outputFilePath = args[i + 1];
+			else if (args[i].equals("-w"))
+				weight = Integer.parseInt(args[i + 1]);
+			else if (args[i].equals("-m"))
+				phraseSize = Integer.parseInt(args[i + 1]);
+			else if (args[i].equals("-n"))
+				resultCnt = Integer.parseInt(args[i + 1]);
+		}
+	}
+}
